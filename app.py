87521acaf1413fd81a76b626b9e689003f4ed4dc
yaroslavasrf <ya.sarafanova@gmail.com>
2025-04-04 21:51:36 +0300

    add 2
    add 2

--- app.py
+++ app.py
@@ -1,10 +1,22 @@
 from flask import Flask, render_template, request
 from config import Config
-from extensions import db
+from extensions import db, login_manager
 from models import User, Cup
 
 app = Flask(__name__)
 app.config.from_object(Config)
+
+db.init_app(app)
+login_manager.init_app(app)
+
+
+@login_manager.user_loader
+def load_user(user_id):
+    return User.query.get(int(user_id))
+
+
+with app.app_context():
+    db.create_all()
 
 
 @app.route('/')
